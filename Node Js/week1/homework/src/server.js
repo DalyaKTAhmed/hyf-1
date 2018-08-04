20f65e0c6cd4799e4f4bc8185ec16f798b9a2ead
Doua Halwa <duaah1980@gmail.com>
2018-08-04 22:23:34 +0200

    home work solved

---
+++ Node Js/week1/homework/src/server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const http = require('http');
+
+/* `createServer` MUST return an instance of `http.Server` otherwise the tests
+ * will fail.
+ */
+function createServer(port) {
+
+  let state = 10;
+  let currentState = null;
+
+  const server = http.createServer((request, response) => {
+
+    response.writeHead(200, { "Content-Type": "application/json" });
+
+    switch (request.url) {
+      case "/state":
+        currentState = JSON.stringify({
+          "state": state
+        });
+        response.end(currentState);
+        break;
+
+      case "/add":
+        state++;
+        currentState = JSON.stringify({
+          "state": state
+        });
+        response.end(currentState);
+        break;
+
+      case "/subtract":
+        state--;
+        currentState = JSON.stringify({
+          "state": state
+        });
+        response.end(currentState);
+        break;
+
+      case "/reset":
+        state = 10;
+        currentState = JSON.stringify({
+          "state": state
+        });
+        response.end(currentState);
+        break;
+
+      default:
+        response.writeHead(404, { "Content-Type": "application/json" })
+        currentState = JSON.stringify({
+          error: 'Not found'
+        });
+        response.end(currentState);
+    }
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer
+};
