30ad9ee2f7530ca32f9e3bb12869e720b09b2b2d
Doua Halwa <duaah1980@gmail.com>
2018-09-23 12:52:29 +0200

    using props

--- React/week1/todo-app/src/TodoList.js
+++ React/week1/todo-app/src/TodoList.js
@@ -4,30 +4,20 @@ import ReactDOM from "react-dom";
 class TodoList extends Component {
 
     constructor(props) {
-        super(props);
-
-        this.state = {
-            tasks: [
+        super();
+            this.tasks = [
                 {
                 description: "Get out of bed,", date:"Wed Sep 13 2017"
-                },
-                {
-                description:"Brush teeth,", date:"Thu Sep 14 2017"
-
-                },
-                {
-                description:"Eat breakfast,", date:"Fri Sep 15 2017"
-                }
-                
-            ] 
-         };
-
+                },  
+            ];
+            this.tasks = this.tasks.concat(props.tasks); 
     };
 
     render() {
+
         return (
             <div className="main">
-                {this.state.tasks.map((todo,index) => <li key={index}> {todo.description} {todo.date}</li>)}
+                {this.tasks.map((todo,index) => <li key={index}> {todo.description} {todo.date}</li>)}
             </div>
 
         );
--- React/week1/todo-app/src/index.js
+++ React/week1/todo-app/src/index.js
@@ -5,11 +5,19 @@ import TodoList from "./TodoList"
 // import  App from "./App.js";
 
 let destination = document.querySelector("#container");
+const tasks = [
+    {
+        description:"Brush teeth,", date:"Thu Sep 14 2017"
+        },
+        {
+        description:"Eat breakfast,", date:"Fri Sep 15 2017"
+        }
+];
 
 ReactDOM.render(
     <div>
         <p> Todo list</p>
-        <TodoList />
+        <TodoList tasks={tasks}/>
         {/* <App/> */}
 
     </div>, destination
