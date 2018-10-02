c1199517faa4e07f51ceec758a04157e100a22ac
Doua Halwa <duaah1980@gmail.com>
2018-10-02 22:35:11 +0200

    refactor

--- React/week2/todo-list/src/App.js
+++ React/week2/todo-list/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from "react";
 
-import TodoList from "./TodoList";
-import todoList from "./todoList.json";
+import Todos from "./Todos";
+import data from "./data.json";
 
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            tasks: todoList.tasks
+            tasks: data,
+            id:13
+            
+            
         };
+        this.handleCheck = this.handleCheck.bind(this);
 
     }
+    handleCheck(e) {
+        // const id = Number(e.target.value);;
+        // console.log(id);
+        // const modifiedData = this.state.data.map(task => {
+        //     if (task.id === id) {
+        //         task.done = !task.done;
+        //     }
+        //     return true;
+        // });
+
+        // this.setState(() => { return modifiedData });
+        // // this.setState(modifiedData); 
+        const id = 17;
+        this.setState(() => {return  id });           
+    };
+
 
     render() {
         if (this.state.tasks.length) {
-            //   let tasks = this.state.tasks;
-            //     tasks = tasks.map((task) => <Task  task={task.description} key={task.id}/>);
             return (
                 <div className="main">
-                    <TodoList tasks={this.state.tasks} />
+                    <Todos tasks={this.state.tasks} handleCheck={(e)=> this.handleCheck(e)} />
                 </div>
             )
         }
---
+++ React/week2/todo-list/src/Todos.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+// import CheckBox from "./CheckBox";
+import "./index.css";
+import Todo from "./Todo"
+
+
+class Todos extends Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    };
+    render() {
+
+        return (
+            <div>
+                {this.props.tasks.map((task) =>
+                    <Todo task={task} key={task.id} handelCheck={this.props.handleCheck} />
+                )}
+            </div>
+        )
+    }
+};
+
+//     render() {
+//         let tasks = this.props.tasks;
+//         let handleCheck = this.props.handleCheck;
+//         return (
+//             <form>
+//                 {tasks.map((task) =>
+//                     <div className="todo-task">
+//                         <input type="checkbox" onChange={handleCheck} />
+//                         <label className="strike-through" key={task.id}>
+//                             <span>{task.description}</span>
+//                             <span>{task.deadline}</span>
+//                         </label>
+//                     </div>
+//                 )}
+//             </form>
+//         )
+//     }
+// };
+
+
+
+export default Todos;
